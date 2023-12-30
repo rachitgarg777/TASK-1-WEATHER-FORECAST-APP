c9e444672cbfe7b52f8d9ae6813bd9614c4cfa2b
rachitgarg777 <147407911+rachitgarg777@users.noreply.github.com>
2023-12-30 17:47:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const locationButton = document.querySelector(".location-btn");
 const currentWeatherDiv = document.querySelector(".current-weather");
 const weatherCardsDiv = document.querySelector(".weather-cards");
 
-const API_KEY = "!"; 
+const API_KEY = "API_KEY"; 
 
 const createWeatherCard = (cityName, weatherItem, index) => {
     if(index === 0) { 
